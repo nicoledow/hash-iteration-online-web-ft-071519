82e2e7a6b648f0319a520f65d90b99db2cad65ca
Nicole Dow <nikki.dow71@gmail.com>
2019-07-16 00:52:14 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # passengers = {
 # suite_a: "Amanda Presley", 
 # suite_b: "Seymour Hoffman", 
@@ -7,5 +9,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
