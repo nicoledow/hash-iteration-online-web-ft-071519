afa817422c694cc783b01dd33db88b3d42912166
Nicole Dow <nikki.dow71@gmail.com>
2019-07-24 19:09:30 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,19 +1,19 @@
-require 'pry'
+# require 'pry'
 
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+# # passengers = {
+# # suite_a: "Amanda Presley", 
+# # suite_b: "Seymour Hoffman", 
+# # suite_c: "Alfred Tennyson", 
+# # suite_d: "Charlie Chaplin", 
+# # suite_e: "Crumpet the Elf"
+# # }
 
-def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
-  end
-  winner
-end
+# def select_winner(passengers)
+#   winner = ""
+#   passengers.each do |suite, name|
+#     if suite == :suite_a && name.start_with?("A")
+#       winner = name
+#     end
+#   end
+#   winner
+# end
